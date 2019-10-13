3330ec8b1a3337049240fb5c081b345005fb377e
Khaled Hazime <hazimekhaled01@gmail.com>
2019-10-13 17:04:31 -0300

    Node verify_door_state publishing to door_status topic

---
+++ src/verify_door_state.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+ 
+import rospy
+from sensor_msgs.msg import LaserScan
+import math
+import statistics
+from std_msgs.msg import Bool
+
+range_calculation = False
+range_position_min = 0
+range_position_max = 0
+closed_door_range = 0.00
+
+def reading_gap(msg):
+    global range_position_min
+    global range_position_max
+    global closed_door_range
+    global range_calculation
+
+    angle_gap = math.radians(rospy.get_param("/angle_gap"))
+    
+    if msg.angle_min < 0:
+        angle_min = msg.angle_min*(-1)
+    else:
+        angle_min = msg.angle_min
+    
+    angle_max = msg.angle_max
+    angle_range = angle_min + angle_max
+    
+    rpr = (len(msg.ranges)/angle_range) #readings per radians
+    range_gap = rpr*angle_gap
+    range_position_min = int((len(msg.ranges)/2)-range_gap)
+    range_position_max = int((len(msg.ranges)/2)+range_gap)
+
+    ranges = msg.ranges[range_position_min:range_position_max+1]
+    ranges_median = statistics.median(ranges)
+    closed_door_range = ranges_median
+    range_calculation = True
+    # print ('angle_max: ')
+    # print (angle_max)
+    # print ('angle_min: ')
+    # print (angle_min)
+    # print ('angle_gap: ')
+    # print (angle_gap)
+    # print ('readings per radians: ')
+    # print (rpr)
+    # print ('range_gap: ')
+    # print (range_gap)
+    # print ('range_position_min: ')
+    # print (range_position_min)
+    # print ('range_position_max: ')
+    # print (range_position_max)
+    # print ('Ranges_median1')
+    # print (ranges_median)
+    
+    
+
+def callback(msg):
+    if range_calculation == False:
+        reading_gap(msg)
+    ranges = msg.ranges[range_position_min:range_position_max+1]
+    ranges_median = statistics.median(ranges)
+    # print ('Closed door range:')
+        
+    if ranges_median > closed_door_range+2:
+        pub.publish(True)
+        print ('door is opened')
+    else:
+        print ('door is closed')
+    
+    
+    
+
+rospy.init_node('verify_door_state')
+sub = rospy.Subscriber('/scan', LaserScan, callback)
+pub = rospy.Publisher('verify_door_state', Bool, queue_size=10)
+reading_gap
+rospy.spin()
